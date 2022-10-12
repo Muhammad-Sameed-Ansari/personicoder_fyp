d367a07d7aa4a4e5cef4d2fc50c46271d9fd0d73
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-10-12 23:17:58 +0500

    Connecting with firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOpso6JsnxheVT225vPXaNCn31SLKyddA",
+  authDomain: "personicoder.firebaseapp.com",
+  projectId: "personicoder",
+  storageBucket: "personicoder.appspot.com",
+  messagingSenderId: "840775144919",
+  appId: "1:840775144919:web:20adb6449a68513c746792"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
