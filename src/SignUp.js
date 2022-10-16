1a32da743ef413e6ca52a7b7094ffcc089dc7a26
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-10-16 22:20:40 +0500

    Firestore implemented on signup

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,16 +1,26 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import React, { useState } from 'react'
-import { auth } from './firebase';
+import { auth, db } from './firebase';
 import Navbar from './Navbar';
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
 
     const handleSignUp = async () =>  {
         try {
-            await createUserWithEmailAndPassword(auth, email, password).then(console.log("Register Success"));
+            const result = await createUserWithEmailAndPassword(auth, email, password).then(console.log("Register Success"));
+			const user = result.user;
+			await setDoc(doc(db, "users", user.uid), {
+				uid: user.uid,
+				name: username,
+				email: email
+			});
+			navigate("/sign-in");
         } catch (err) {
             console.error(err);
             alert(err.message);
