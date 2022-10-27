a55f83f11c65221c5ad3142c72e26b0f0c16fb8d
Muhammad-Sameed-Ansari <sameedanxari@gmail.com>
2022-10-27 17:28:46 +0500

    Chat connected with firebase

--- src/ChatBox.js
+++ src/ChatBox.js
@@ -1,15 +1,57 @@
-import { MoreVert } from '@mui/icons-material';
+import { MicOutlined, MoreVert } from '@mui/icons-material';
 import { Avatar, IconButton } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar';
 import './ChatBox.css'
-import SendIcon from '@mui/icons-material/Send';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from './firebase';
+import { collection, doc, getDoc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from 'firebase/firestore';
 
 export default function ChatBox() {
-    const [user] = useAuthState(auth);
+    const [username, setUsername] = useState("");
+    const [input, setInput] = useState("")
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        fetchUserName();
+        fetchMessages();
+    }, [username])
+
+    const fetchUserName = async ()  => {
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            setUsername(docSnap.data().name);
+        } else {
+            console.log("No document found");
+        }
+    }
+
+    const fetchMessages = async () => {
+        const q = query(collection(db, 'users', auth.currentUser.uid, "messages"), orderBy("timestamp", "asc"));
+        onSnapshot(q, (snapshot) => {
+            
+            setMessages(snapshot.docs.map((doc) => doc.data(), doc.id))
+        })
+        
+    }
     
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        console.log(input)
+        console.log(auth.currentUser.uid);
+
+        const userRef = collection(db, 'users', auth.currentUser.uid, "messages");
+
+        await setDoc(doc(userRef), {
+            message: input,
+            name: username,
+            timestamp: serverTimestamp()
+        });
+        setInput("");
+    }
+
     return (
         <div>
             <Navbar islogin={true} />
@@ -17,7 +59,7 @@ export default function ChatBox() {
                 <Avatar />
 
                 <div className='chat__headerInfo'>
-                    <h3>{user.displayName}</h3>
+                    <h3>{username}</h3>
                 </div>
 
                 <div className='chat__headerRight'>
@@ -28,52 +70,28 @@ export default function ChatBox() {
             </div>
 
             <div className='chat__body'>
-                <p className='chat__message'>
-                    <span className='chat__name'>Saif</span>
-                    This is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
-
-                <p className='chat__message chat__reciever'>
-                    <span className='chat__name'>Saif</span>
-                    This is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Saif</span>
-                    This is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Saif</span>
-                    This is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
-
-                <p className='chat__message chat__reciever'>
-                    <span className='chat__name'>Saif</span>
-                    This is a messageThis is a messageThis is a messageThis is a messageThis is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
-
-                <p className='chat__message'>
-                    <span className='chat__name'>Saif</span>
-                    This is a message
-                    <span className='chat__timestamp'>{new Date().toUTCString()}</span>
-                </p>
+                {messages.map((message) => (
+                    <p className={`chat__message ${message.name === username && "chat__reciever"}`}>
+                        <span className='chat__name'>{message.name}</span>
+                        {message.message}
+                        <span className='chat__timestamp'>{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+                    </p>
+                ))}
             </div>
 
             <div className='chat__footer'>
+                <form>
                 <input 
                     placeholder='Type a message'
                     type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
-                <button type="submit">
+                <button type="submit" onClick={sendMessage}>
                     Send a message
                 </button>
-                <SendIcon />
+                </form>
+                <MicOutlined />
             </div>
         </div>
     )
